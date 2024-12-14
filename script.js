8986fc2e3d41c8a01fe7db34302eb61b32e672ca
Avishka Priyasad <avishkapriyasad11@gmail.com>
2024-12-14 16:11:06 +0530

    Update Animated text

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ for (var i = 0; i < navLinks.length; i++) {
 // Animation
 document.addEventListener('DOMContentLoaded', function () {
   var typed = new Typed(".auto-type", {
-    strings: ["Undergraduate", "Front-end Developer", "DevOps Enthusiast"],
+    strings: ["Front-end Developer", "DevOps Engineer"],
     typeSpeed: 70,
     backSpeed: 70,
     loop: true
